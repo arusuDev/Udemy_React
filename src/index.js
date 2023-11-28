0ae8a4ffc01f9bdb0840a1aa29befe7e273f542b
arusuDev <arse.mria@gmail.com>
2023-11-28 15:34:03 +0900

    Refactor Code by Dividing Functions for Better Modularity

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const onClickAdd = () => {
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
 
+    createIncompleteTodo(inputText);
+}
+
+// 渡された引数を元に未完了のTODOを作成する関数
+const createIncompleteTodo = (todo) => {
     // li生成
     const li = document.createElement("li");
     // div生成 (class list-row)
@@ -11,11 +16,27 @@ const onClickAdd = () => {
     // p生成 (class todo-item)
     const p = document.createElement("p");
     p.className = "todo-item";
-    p.innerText = inputText;
+    p.innerText = todo;
     // button(完了)作成
     const completeButton = document.createElement("button");
     completeButton.innerText = "完了";
-    completeButton.addEventListener("click",() => alert("完了"));
+    completeButton.addEventListener("click",() => {
+        // 押された完了ボタンの親にあるliタグを取得する
+        const moveTarget = completeButton.closest("li");
+        // 自分自身の次の要素を削除する。(削除ボタン)
+        completeButton.nextElementSibling.remove();
+        // 自分自身を削除する。
+        completeButton.remove();
+        // 戻すボタンを生成してdivタグ配下に設定する
+        const backButton = document.createElement("button");
+        backButton.innerText = "戻す";
+        // 最初の子要素を取得(firstElementChild)
+        moveTarget.firstElementChild.appendChild(backButton);
+
+        // 完了リストに移動
+        // moveTargetは参照で持っているため、appendChildで項目を移動できる
+        document.getElementById("complete-list").appendChild(moveTarget);
+    });
 
     // button(削除)作成
     const deleteButton = document.createElement("button");
@@ -34,7 +55,6 @@ const onClickAdd = () => {
     
     // id=imcomplete-listの下に要素を追加
     document.getElementById("imcomplete-list").appendChild(li);
-
 }
 
 // add-button idを持つ要素を取得してclickされたときにonClickAddを呼び出す
