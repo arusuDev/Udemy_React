064ac93929c97e9ef51d6160a40993240e91a65b
arusuDev <arse.mria@gmail.com>
2023-11-29 01:06:37 +0900

    Refactor index.js to create App component

--- react-todo/src/index.js
+++ react-todo/src/index.js
@@ -4,8 +4,10 @@ import { createRoot } from "react-dom/client";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const App = () => <h1>Hello React</h1>;
+
 root.render(
   <StrictMode>
-    <h1>Hello React!</h1>
+    <App />
   </StrictMode>
 );
