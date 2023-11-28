966b6472eaa28941f0b8fdf7f1ec7d37734a0d6d
arusuDev <arse.mria@gmail.com>
2023-11-28 15:00:21 +0900

    Add Code for Appending Lists in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const onClickAdd = () => {
-    alert();
+    // 入力された文字を取得し、ボックスを初期化する
+    const inputText = document.getElementById("add-text").value;
+    document.getElementById("add-text").value = "";
+
+    // li生成
+    const li = document.createElement("li");
+    // div生成 (class list-row)
+    const div = document.createElement("div");
+    div.className = "list-row";
+    // p生成 (class todo-item)
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = inputText;
+
+    // 階層構造の作成 li -> div -> p
+    div.appendChild(p);
+    li.appendChild(div);
+    
+    // id=imcomplete-listの下に要素を追加
+    document.getElementById("imcompleate-list").appendChild(li);
+
 }
 
 // add-button idを持つ要素を取得してclickされたときにonClickAddを呼び出す
