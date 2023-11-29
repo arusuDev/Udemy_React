409561a294c6dd17f5d3061b5a260688c6193db3
arusuDev <arse.mria@gmail.com>
2023-11-30 00:29:39 +0900

    CSS inline style sample

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,9 +1,18 @@
 export const App = () => {
   const onClickButton = () => alert();
+  // CSS インラインスタイル
+  // Reactで記載する場合はcamelCaseで書くこと。
+  // fontsize -> fontSize
+  // 数値は数値として記載できるが、オブジェクトとして使うため、
+  // colorコードなどは文字列として定義する。
+  const contentStyle = {
+    color: "blue",
+    fontSize: "3rem",
+  };
 
   return (
     <div>
-      <h1>Hello React!</h1>
+      <h1 style={contentStyle}>Hello React!</h1>
       <button onClick={onClickButton}>ボタン</button>
     </div>
   );
