116147178f5a8e16710652a7fb9aee54a4c613e4
arusuDev <arse.mria@gmail.com>
2023-12-01 18:09:17 +0900

    Update List styling format in React component

--- react-todo/src/ToDo.jsx
+++ react-todo/src/ToDo.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
 import "./style.css";
 
 export const ToDo = () => {
+  const [imcompleteToDos, setIncompleteTodos] = useState([
+    "TODOです1",
+    "TODOです2",
+  ]);
+
+  const [completeToDos, setCompleteToDos] = useState([
+    "完了したTODOです1",
+    "完了したTODOです2",
+  ]);
+
   return (
     <>
       <div className="input-area">
@@ -10,24 +21,29 @@ export const ToDo = () => {
       <div className="imcompleate-area">
         <p className="area-title">未完了のToDo</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p>TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {imcompleteToDos.map((todo) => (
+            // 一意になる値を設定する
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="completed-area">
         <p className="area-title">完了したToDo</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p className="todo-item">TODOでした</p>
-              <button>戻す</button>
-            </div>
-          </li>
+          {completeToDos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>戻す</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </>
