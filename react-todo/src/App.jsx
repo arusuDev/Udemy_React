00e7dfd4b4f2b9408a861e690ec3c452921f3c9a
arusuDev <arse.mria@gmail.com>
2023-12-01 17:41:42 +0900

    Add : sample of useEffect

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
   // useStateは関数コンポーネントの一番上の階層でしか呼び出せない。
   const [num, setNum] = useState(0);
+  const [isShowChar, setIsShowChar] = useState(true);
   const onClickCountUp = () => {
     setNum((prev) => prev + 1);
-    setNum((prev) => prev + 1);
   };
+  const onClickToggle = () => {
+    setIsShowChar(!isShowChar);
+  };
+
+  // useEffectは第二引数の配列の値が変更かかった場合のみ実行される。
+  useEffect(() => {
+    if (num % 3 === 0 && num > 0) {
+      isShowChar || setIsShowChar(true);
+    } else {
+      isShowChar && setIsShowChar(false);
+    }
+  }, [num]);
+
+  // Too many re-renders.
+  // if (num % 3 === 0) {
+  //   setIsShowChar(true);
+  // } else {
+  //   setIsShowChar(false);
+  // }
+
   return (
     <div>
       <ColorfulMessage color="red"> This is Children </ColorfulMessage>
@@ -15,6 +35,8 @@ export const App = () => {
       <ColorfulMessage color="green"> Hello,React! </ColorfulMessage>
       <button onClick={onClickCountUp}>CountUp</button>
       <p>State:{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowChar && <p>moji</p>}
     </div>
   );
 };
